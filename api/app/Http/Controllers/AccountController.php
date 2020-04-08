f272844f6722195ffbc62dbee3f422f432c2f18c
Your Name <brunorb8@hotmail.com>
2020-04-08 03:04:06 -0300

    account controller: createTransaction, list and find endpoints

---
+++ api/app/Http/Controllers/AccountController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Account as ResourcesAccount;
+use App\Http\Resources\Transaction as ResourcesTransaction;
+use App\Models\Account;
+use App\Models\Currency as C;
+use App\Models\Transaction;
+use Money\Money;
+use Money\Currency as MoneyCurrency;
+
+
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+
+    public function createTransaction(Request $request, Account $account)
+    {
+        $validatedData = $request->validate([
+            'to' => 'required|integer|min:1',
+            'details' => 'nullable|string',
+            'amount' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) {
+                    if ($value <= 0) {
+                        $fail($attribute.' should be a positive value.');
+                    }
+                },
+            ]
+        ]);
+
+        $transferAmount = new Money(
+            C::toCents($validatedData['amount']),
+            new MoneyCurrency($account->currency->iso_code)
+        );
+
+        $to = Account::findOrFail($request['to']);
+
+        if ($account->id === $to->id) {
+            abort(400, 'Cannot transfer from one account to itself');
+        } elseif ($transferAmount->greaterThan($account->balance)) {
+            abort(400, 'Not enough money to transfer');
+        }
+
+        $t = Transaction::makeTransaction($transferAmount, $account, $to);
+        return new ResourcesTransaction($t);
+    }
+
+    public function list(Request $request, Account $account)
+    {
+        $xs = $account
+            ->all_transactions()
+            ->orderBy('created_at', 'DESC')
+            ->paginate(20);
+
+        return ResourcesTransaction::collection($xs);
+    }
+
+    public function find(Request $request, Account $account)
+    {
+        return new ResourcesAccount($account);
+    }
+}
